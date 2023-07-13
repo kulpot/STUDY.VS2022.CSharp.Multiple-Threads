5381829d912eb9449dc3d00c349ce9b12c98a3d2
Kulpot <kulpotwahu@gmail.com>
2023-07-14 02:49:26 +0800

    Add project files.

---
+++ Multiple Threads/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+//ref link:https://www.youtube.com/watch?v=8Ji3C_x6r1U&list=PLRwVmtr-pp06KcX24ycbC-KkmAISAFKV5&index=5
+// Threading -
+
+class MainClass
+{
+    static void Main()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            var thread = new Thread(DifferentMethod);
+            thread.Start(i);
+        }
+        /*var thread = new Thread(DifferentMethod);
+        thread.Start();
+        while (true)
+            Console.WriteLine("Hello from Main()");*/
+    }
+    static void DifferentMethod(object threadID)
+    {
+        while(true)
+            Console.WriteLine("Hello from different method: " + threadID);
+    }
+}
